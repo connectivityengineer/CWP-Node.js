9935222813055ce16afb2b1995c0ab116d954dee
Glenn Kelley <37850634+connectivityengineer@users.noreply.github.com>
2018-06-27 22:48:12 -0400

    Update nodejs.php

--- nodejs.php
+++ nodejs.php
@@ -17,14 +17,14 @@
                 <form method="post">
                         <table cellpadding="5" border="0">
                                 <tr>
-                                        <td align="right">Version:</td><td align="left"><select style="width: 100px;" name="setup"><option value="6">Node.js v6</option><option value="8">Node.js v8</option></select></td>
+                                        <td align="right">Version:</td><td align="left"><select style="width: 100px;" name="setup"><option value="6">Node.js v6</option><option value="8">Node.js v8</option><option value="10">Node.js v10</option></select></td>
                                 </tr>
                                 <tr>
                                         <td></td><td align="center"><button name="installNode">Install Node.js</button></td>
                                 </tr>
                         </table>
                 </form>
-				<center>Script by <a href="https://www.born2host.com" target="_blank">Born 2 Host</a></center>
+				<center>Script originally by <a href="https://www.born2host.com" target="_blank">Born 2 Host</a></center>
         </div>
 </center>
 </pre>
